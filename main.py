3ac43f288485e1a9c808c3dd9ce2ff6068ca738c
Peter Nagymathe <peter@nagymathe.xyz>
2023-09-09 15:49:33 +0000

    now with better command line usage

--- main.py
+++ main.py
@@ -2,15 +2,21 @@
 # Example:
 # randomuser - "!echo example string"
 # echo_bot - "example string"
-from asyncio import sleep
+import markdown2
+import logging
 import asyncio
+from dataclasses import dataclass
+import re
 import nio
 import simplematrixbotlib as botlib
-from atro_args import InputArgs, Arg
+from atro_args import InputArgs, Arg, arg_source
 
 from my_bot import matrix_helper, openai_helper
 from my_bot import ai_types
 from my_bot.ai_types import ChatCompletion, ChatCompletionMessage
+
+def get_args(x: str) -> list[str]:
+    return [y.strip('"') for y in re.findall(r'(?:[^\s"]+|"[^"]*")', x)]
 
 input_args = InputArgs(
     prefix="MATRIX_BOT_CGPT", 
@@ -18,17 +24,48 @@ input_args = InputArgs(
         Arg(name = "HOME"),
         Arg(name = "USERNAME"),
         Arg(name = "PASSWORD"),
-        Arg(name = "CREDPATH", required = False, default = "./secrets/session.txt"),
         Arg(name = "CGPT_TOKEN"),
+        Arg(name = "CREDPATH", required = False, default = "./secrets/session.txt"),
     ]
 )
-args = input_args.get_dict()
+input_args_parsed = input_args.get_dict()
 
-home_url: str = args["HOME"]
-creds = botlib.Creds(home_url, args["USERNAME"], args["PASSWORD"], session_stored_file=args["CREDPATH"])
+home_url: str = input_args_parsed["HOME"]
+creds = botlib.Creds(home_url, input_args_parsed["USERNAME"], input_args_parsed["PASSWORD"], session_stored_file=input_args_parsed["CREDPATH"])
 bot = botlib.Bot(creds)
 PREFIX = '!'
-llm: ai_types.AsyncOpenLlm = openai_helper.LocalLlama(args["CGPT_TOKEN"], "http://localhost:8000")
+llm: ai_types.AsyncOpenLlm = openai_helper.LocalLlama(input_args_parsed["CGPT_TOKEN"], "http://192.168.0.28:8000")
+
+@dataclass
+class CgptArgs:
+    message: str
+    model: str = "gpt-3.5-turbo"
+    temperature: float = 0.9
+    max_tokens: int = -1
+    top_p: float = 1
+    frequency_penalty: float = 0
+    presence_penalty: float = 0
+    n: int = 1
+    user: str = "user"
+    run: bool = True
+
+atro_parser = InputArgs(
+    args = [
+        Arg(name = "message", other_names=["m"], arg_type = str, required = True),
+        Arg(name = "model", other_names=["ml"], arg_type = str, required = False, default="gpt-3.5-turbo"),
+        Arg(name = "temperature", other_names=["t"], arg_type = float, required = False, default=0.9),
+        Arg(name = "max_tokens", other_names=["mt"], arg_type = int, required = False, default=-1),
+        Arg(name = "top_p", other_names=["p"], arg_type = float, required = False, default=1),
+        Arg(name = "frequency_penalty", other_names=["fp"], arg_type = float, required = False, default=0),
+        Arg(name = "presence_penalty", other_names=["pp"], arg_type = float, required = False, default=0),
+        Arg(name = "n", other_names=["n"], required = False, arg_type = int, default=1),
+        Arg(name = "user", other_names=["u"], required = False, arg_type = str, default="user"),
+        Arg(name = "run", other_names=["r"], required = False, arg_type = bool, default=True)
+    ],
+    sources = [
+        arg_source.ArgSource.cli_args
+    ]
+)
 
 @bot.listener.on_message_event
 async def cmd_exit(room: nio.rooms.MatrixRoom, message: nio.RoomMessageText):
@@ -44,6 +81,7 @@ def cli_help(bot: botlib.Bot, room: nio.rooms.MatrixRoom):
     Commands:
     !exit - exits the program
     !cgpt - runs the cgpt command
+    !clear - clear message history
     !help - shows this help message
         """
     )
@@ -51,63 +89,90 @@ def cli_help(bot: botlib.Bot, room: nio.rooms.MatrixRoom):
 @bot.listener.on_message_event
 async def cmd_help(room: nio.rooms.MatrixRoom, message: nio.RoomMessageText):
     if(message.sender == creds.username): return
-    if(message.body.startswith("!help")):
+    if(message.body.startswith(f"{PREFIX}help")):
         await cli_help(bot, room)
 
-def clean_message(message: str) -> str:
-    if not message.startswith("!cgpt"): return message
-    return " ".join(message.split(" ")[2:])
+@bot.listener.on_message_event
+async def cmd_clear(room: nio.rooms.MatrixRoom, message: nio.RoomMessageText):
+    if(message.sender == creds.username): return
 
-async def cli_message(bot: botlib.Bot, room: nio.rooms.MatrixRoom, args: list[str]):
-    await bot.api.async_client.room_typing(room.room_id) # start typing indicator
-
+async def cli_message2(bot: botlib.Bot, room: nio.rooms.MatrixRoom, args: CgptArgs):
+    await bot.api.async_client.room_typing(room.room_id, timeout=1000 * 120) # start typing indicator
     history: list[ChatCompletionMessage] = []
-    async for x in matrix_helper.recursive_message(bot, room, 10, 10):
-        if x[0] is not None: break
-        if not isinstance(x[1], nio.RoomMessageText): continue
-        if x[1].sender == creds.username:
-            history.append(ChatCompletionMessage(role="assistant", content=clean_message(x[1].body)))
+    
+    async for result in matrix_helper.recursive_message(bot, room, 100, 10):
+        if result[0] is not None: break
+        if len(history) >= 10: break
+
+        msg = result[1]
+        if not isinstance(msg, nio.RoomMessageText): continue
+        if msg.body.startswith(f"{PREFIX}ERROR"): continue
+        if msg.body.startswith(f"{PREFIX}DEBUG"): continue
+
+        # message from cgpt
+        if msg.sender == creds.username:
+            history.append(ChatCompletionMessage(role="assistant", content=msg.body))
             continue
-        if not x[1].body.startswith("!cgpt"): continue
-        __args = x[1].body.split(" ")[1:]
-        if __args[0] == "clear": break
-        if not __args[0] == "message": continue
-        history.append(ChatCompletionMessage(role="user", content=clean_message(x[1].body)))
+        # not a cgpt request
+        if msg.body.startswith(f"{PREFIX}clear"): break
+        if not msg.body.startswith(f"{PREFIX}cgpt"): continue
+
+        # we have cgpt request
+        input_str = " ".join(msg.body.split(" ")[1:])
+        split_args: list[str] = get_args(input_str)
+        try:
+            __cargs = CgptArgs(**atro_parser.get_dict(split_args))
+        except Exception as e:
+            logging.error(f"!ERROR\nIgnoring message due to parsing error:\n{e}\n\nMessage:\n{msg.body}")
+            continue
+        logging.debug(f"!DEBUG\nParsed args:\n{__cargs}")
+        history.append(ChatCompletionMessage(role="user", content=__cargs.message))
     history.reverse()
 
+    logging.debug(f"!DEBUG\nHistory:\n{history}")
     completion: ChatCompletion = await llm.acreate_chat_completion(
             history
         ) # type: ignore
-
-    # completion: ChatCompletion = llm.create_chat_completion(
-    #         history
-    #     ) # type: ignore
-    await bot.api.send_text_message(
-            room.room_id, completion["choices"][0]["message"]["content"]
-        )
+    logging.debug(f"!DEBUG\nCompletion:\n{completion}")
+    msg = completion["choices"][0]["message"]["content"]
+    await bot.api._send_room(
+        room.room_id,
+        {
+            "msgtype": "m.text",
+            "body": msg,
+            "format": "org.matrix.custom.html",
+            "formatted_body": markdown2.markdown(msg)
+        }
+    )
     await bot.api.async_client.room_typing(room.room_id, False) # start typing indicator
 
 @bot.listener.on_message_event
 async def cmd_completion(room: nio.rooms.MatrixRoom, message: nio.RoomMessageText):
     if(message.sender == creds.username): return
-    if(not message.body.startswith("!cgpt")): return
+    if(not message.body.startswith(f"{PREFIX}cgpt")): return
 
     # check for commands
-    args = message.body.split(" ")[1:]
-    if(len(args) == 0):
+    tmp = message.body.split(" ")[1:]
+    if(len(tmp) == 0):
         await cli_help(bot, room)
         return
-    match args[0]:
-        case "help":
-            return await cli_help(bot, room)
-        case "exit":
-            await bot.api.send_text_message(room.room_id, "Exiting...")
-            exit()
-        case "message":
-            return await cli_message(bot, room, args[2:])
-        case _:
-            return
+    
+    ## Running cgpt
+    input_str = " ".join(tmp)
+    split_args: list[str] = get_args(input_str)
+    logging.debug(f"!DEBUG\nRaw args:\n{split_args}")
+    try:
+        __dict = atro_parser.get_dict(split_args)
+    except Exception as e:
+        logging.debug(f"!DEBUG\nIgnoring message due to parsing error:\n{e}\n\nMessage:\n{message.body}")
+        return
+    __arg = CgptArgs(**__dict)
+    if __arg.user != "user":
+        return
+    if not __arg.run:
+        return
+    logging.debug(f"{PREFIX}DEBUG:\nRunning cgpt... with atro args with dataclass:\n{__arg}")
+    return await cli_message2(bot, room, __arg)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(bot.main())
-# bot.run()
--- my_bot/openai_helper.py
+++ my_bot/openai_helper.py
@@ -13,6 +13,31 @@ class GPT():
         openai.api_key = TOKEN
         if url is not None:
             openai.api_base = url
+    
+    async def acreate_chat_completion(
+        self,
+        messages: list[ChatCompletionMessage],
+        model: str = "gpt-3.5-turbo",
+        temperature: float = 0.9,
+        max_tokens: int = -1,
+        top_p: float = 1,
+        frequency_penalty: float = 0,
+        presence_penalty: float = 0,
+        n: int = 1,
+        stream: bool = False
+    ) -> Iterator[ChatCompletionChunk] | ChatCompletion:
+        return await openai.ChatCompletion.acreate(
+            messages = messages,
+            model = model,
+            temperature = temperature,
+            max_tokens = max_tokens,
+            top_p = top_p,
+            frequency_penalty = frequency_penalty,
+            presence_penalty = presence_penalty,
+            n = n,
+            stream = stream
+        ) # type: ignore
+        
 
     def create_chat_completion(
         self,
